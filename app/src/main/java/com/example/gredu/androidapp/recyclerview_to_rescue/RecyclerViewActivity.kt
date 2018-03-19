7a6b8e1cd7b82e62b08ad15d0fe5c8a723416c08
adamluthfi <adamluthfi6@gmail.com>
2018-03-19 12:06:55 +0700

    Add Agenda View

---
+++ app/src/main/java/com/example/gredu/androidapp/recyclerview_to_rescue/RecyclerViewActivity.kt
@@ -0,0 +1,13 @@
+package com.example.gredu.androidapp.recyclerview_to_rescue
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gredu.androidapp.R
+
+class RecyclerViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recyclerview)
+    }
+}
