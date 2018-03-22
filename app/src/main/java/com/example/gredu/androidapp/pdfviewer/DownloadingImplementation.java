49977d93704f25e61891a69389de1a18d94bb3b8
adamluthfi <adamluthfi6@gmail.com>
2018-03-22 14:16:38 +0700

    Add Pdf View

--- app/src/main/java/com/example/gredu/androidapp/pdfviewer/DownloadingImplementation.java
+++ app/src/main/java/com/example/gredu/androidapp/pdfviewer/DownloadingImplementation.java
@@ -146,6 +146,7 @@ public class DownloadingImplementation implements Downloading {
     private boolean isOnline(Context context) {
         ConnectivityManager cm =
                 (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        assert cm != null;
         NetworkInfo netInfo = cm.getActiveNetworkInfo();
         return netInfo != null && netInfo.isConnectedOrConnecting();
     }
